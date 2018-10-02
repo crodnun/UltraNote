f3483409c9e4c7e63f4fb103d23ff4c32362cd2a
Eliott Hauteclair <me@eliott.be>
2018-10-02 18:48:35 +0200

    Burning bug Pt. 2

--- src/Transfers/TransfersConsumer.cpp
+++ src/Transfers/TransfersConsumer.cpp
@@ -8,8 +8,10 @@
 #include "TransfersConsumer.h"
 
 #include <numeric>
+#include <future>
 
 #include "CommonTypes.h"
+#include "Common/StringTools.h"
 #include "Common/BlockingQueue.h"
 #include "CryptoNoteCore/CryptoNoteFormatUtils.h"
 #include "CryptoNoteCore/TransactionApi.h"
@@ -17,9 +19,12 @@
 
 #include "IWallet.h"
 #include "INode.h"
-#include <future>
 
 using namespace Crypto;
+using namespace Logging;
+using namespace Common;
+
+std::unordered_map<Hash, std::vector<PublicKey>> public_keys_seen;
 
 namespace {
 
@@ -414,6 +419,21 @@ std::error_code createTransfers(
 
       assert(out.key == reinterpret_cast<const PublicKey&>(in_ephemeral.publicKey));
 
+      std::unordered_map<Hash, std::vector<PublicKey>>::iterator it = public_keys_seen.find(tx.getTransactionHash());
+ 	    if (it == public_keys_seen.end()) {
+ 		   for (const std::pair<Hash, std::vector<PublicKey>>& kv : public_keys_seen) {
+ 			  auto& keys = kv.second;
+ 		    if (std::find(keys.begin(), keys.end(), out.key) != keys.end()) {
+ 			   throw std::runtime_error("duplicate transaction output key is found");
+ 				 return std::error_code();
+ 			  }
+ 		   } 
+ 	  
+ 		   std::vector<PublicKey> temp_keys;
+ 		   temp_keys.push_back(out.key);
+ 		   public_keys_seen.insert(std::make_pair(tx.getTransactionHash(), temp_keys));
+ 	    }
+
       info.amount = amount;
       info.outputKey = out.key;
 
@@ -422,6 +442,22 @@ std::error_code createTransfers(
       MultisignatureOutput out;
       tx.getOutput(idx, out, amount);
 
+      for (const auto& key : out.keys) {
+ 		  std::unordered_map<Hash, std::vector<PublicKey>>::iterator it = public_keys_seen.find(tx.getTransactionHash());
+ 		  if (it == public_keys_seen.end()) {
+ 			 for (const std::pair<Hash, std::vector<PublicKey>>& kv : public_keys_seen) {
+ 			  auto& keys = kv.second;
+ 			  if (std::find(keys.begin(), keys.end(), key) != keys.end()) {
+ 				  throw std::runtime_error("duplicate transaction output key is found");
+ 				  return std::error_code();
+ 			  }
+ 			}
+
+ 			  std::vector<PublicKey> temp_keys;
+ 			  temp_keys.push_back(key);
+ 			  public_keys_seen.insert(std::make_pair(tx.getTransactionHash(), temp_keys));
+ 		  }
+ 	  }
       info.amount = amount;
       info.requiredSignatures = out.requiredSignatureCount;
       info.term = out.term;
@@ -435,7 +471,13 @@ std::error_code createTransfers(
 
 std::error_code TransfersConsumer::preprocessOutputs(const TransactionBlockInfo& blockInfo, const ITransactionReader& tx, PreprocessInfo& info) {
   std::unordered_map<PublicKey, std::vector<uint32_t>> outputs;
-  findMyOutputs(tx, m_viewSecret, m_spendKeys, outputs);
+  try {
+     findMyOutputs(tx, m_viewSecret, m_spendKeys, outputs);
+   }
+   catch (const std::exception& e) {
+     //m_logger(ERROR, BRIGHT_RED) << "Failed to process transaction: " << e.what() << ", transaction hash " << Common::podToHex(tx.getTransactionHash());
+     return std::error_code();
+   }
 
   if (outputs.empty()) {
     return std::error_code();
@@ -454,10 +496,16 @@ std::error_code TransfersConsumer::preprocessOutputs(const TransactionBlockInfo&
     auto it = m_subscriptions.find(kv.first);
     if (it != m_subscriptions.end()) {
       auto& transfers = info.outputs[kv.first];
-      errorCode = createTransfers(it->second->getKeys(), blockInfo, tx, kv.second, info.globalIdxs, transfers);
-      if (errorCode) {
-        return errorCode;
-      }
+      try {
+ 		   errorCode = createTransfers(it->second->getKeys(), blockInfo, tx, kv.second, info.globalIdxs, transfers);
+ 		   if (errorCode) {
+ 			  return errorCode;
+ 		   }
+ 	    }
+ 	    catch (const std::exception& e) {
+ 		    //m_logger(ERROR, BRIGHT_RED) << "Failed to process transaction: " << e.what() << ", transaction hash " << Common::podToHex(tx.getTransactionHash());
+ 		    return std::error_code();
+ 	   }
     }
   }
 
